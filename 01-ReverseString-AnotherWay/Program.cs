bb2d5775fe7f2c5c6a644afa942e0749cdb1e7bd
denisYordanov-eng <denisivov437@gmail.com>
2025-05-25 12:38:15 +0300

    Made the firstTaskUsing StringBuilder.

---
+++ 01-ReverseString-AnotherWay/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ReverseString_AnotherWay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string  word = Console.ReadLine();
+            StringBuilder  sb = new StringBuilder();
+            for (int i = word.Length -1; i >= 0; i--)
+            {
+                sb.Append(word[i]);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
