9614ab939ab4aa9e92f6523fa474c28d5f65b153
denisYordanov-eng <denisivov437@gmail.com>
2025-05-26 06:53:50 +0300

    Made a task.

---
+++ ConvertFromBase10ToBase-N/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertFromBase10ToBase_N
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ');
+
+            int @base = int.Parse(numbers[0]);
+
+            var num = BigInteger.Parse(numbers[1]);
+
+            StringBuilder sb =  new StringBuilder();
+            while (num > 0)
+            {
+                var remainder = num % @base;
+                sb.Append(remainder);
+                num /= @base;
+            }
+            string reversed = new string
+                (sb.ToString().Reverse().ToArray());
+            Console.WriteLine(reversed);
+        }
+    }
+}
