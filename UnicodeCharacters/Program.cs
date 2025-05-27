1c7ad70089497d5d6243ce022bf29da30cfd0c76
denisYordanov-eng <denisivov437@gmail.com>
2025-05-27 06:57:32 +0300

    Made a task.

---
+++ UnicodeCharacters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicodeCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputWord = Console.ReadLine();
+
+            
+            for (int i = 0; i < inputWord.Length; i++)
+            {
+                int letter = (int)(inputWord[i]);
+               Console.Write("\\u{0:x4}",letter);
+            }  
+          
+        }
+    }
+}
